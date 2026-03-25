3b27438abd4a1ed039f76b45bce7a9fe97965ee3
davinkhor <73937826+deltaMinor@users.noreply.github.com>
2026-03-25 11:59:38 +0800

    refactor: Add FileCardDetails record

--- src/main/java/seedu/address/ui/FileCard.java
+++ src/main/java/seedu/address/ui/FileCard.java
@@ -10,19 +10,13 @@ import java.util.Optional;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
-import seedu.address.commons.exceptions.DataLoadingException;
-import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.timepoint.DateTimeUtil;
-import seedu.address.storage.JsonAddressBookStorage;
 
 /**
  * A UI component that displays information of a B2B4U file.
  */
 public class FileCard extends UiPart<Region> {
     private static final String FXML = "FileListCard.fxml";
-    private static final ZoneId ZONE_ID = ZoneId.systemDefault();
-
-    public final File file;
 
     @FXML
     private Label name;
@@ -34,21 +28,10 @@ public class FileCard extends UiPart<Region> {
     /**
      * Creates a {@code FileCard} with the given file to display.
      */
-    public FileCard(File file) {
+    public FileCard(FileCardDetails details) {
         super(FXML);
-        this.file = file;
-        name.setText(file.getName());
-        LocalDateTime lastModifiedTime = Instant.ofEpochMilli(file.lastModified()).atZone(ZONE_ID).toLocalDateTime();
-        lastModified.setText("Last modified: " + DateTimeUtil.toDisplayString(lastModifiedTime));
-        JsonAddressBookStorage addressBookStorage = new JsonAddressBookStorage(Path.of(file.getPath()));
-        try {
-            Optional<ReadOnlyAddressBook> addressBookOptional = addressBookStorage.readAddressBook();
-            if (!addressBookOptional.isPresent()) {
-                contactCount.setText("0");
-            }
-            contactCount.setText(addressBookOptional.get().getContactList().size() + "");
-        } catch (DataLoadingException e) {
-            contactCount.setText("Error retrieving contact list");
-        }
+        name.setText(details.name());
+        lastModified.setText("Last modified: " + DateTimeUtil.toDisplayString(details.lastModifiedTime()));
+        contactCount.setText(details.contactCount() + "");
     }
 }
--- src/main/java/seedu/address/ui/FileListPanel.java
+++ src/main/java/seedu/address/ui/FileListPanel.java
@@ -8,10 +8,14 @@ import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Logger;
 
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
@@ -23,6 +27,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.DataLoadingException;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.storage.JsonAddressBookStorage;
@@ -33,12 +38,14 @@ import seedu.address.storage.JsonAddressBookStorage;
 public class FileListPanel extends UiPart<Region> {
     private static final String FXML = "FileListPanel.fxml";
     private static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
+    private static final ZoneId ZONE_ID = ZoneId.systemDefault();
 
     private final WatchService watchService;
-    private final ObservableList<File> fileList = FXCollections.observableArrayList();
+    private final ObservableList<FileCardDetails> fileDetailList = FXCollections.observableArrayList();
+    private final Logger logger = LogsCenter.getLogger(ContactListPanel.class);
 
     @FXML
-    private ListView<File> fileListView;
+    private ListView<FileCardDetails> fileListView;
 
     /**
      * Creates a {@code FileListPanel} with the given directory.
@@ -47,10 +54,64 @@ public class FileListPanel extends UiPart<Region> {
         super(FXML);
         File[] existingFiles = directory.toFile().listFiles();
         if (existingFiles != null) {
-            fileList.addAll(existingFiles);
+            for (File file : existingFiles) {
+                try {
+                    fileDetailList.add(getFileCardDetails(file));
+                } catch (Exception e) {
+                    logger.warning(e.getMessage());
+                }
+            }
         }
 
         watchService = FileSystems.getDefault().newWatchService();
+        setUpWatcher(directory);
+
+        SortedList<FileCardDetails> sortedFileDetails = new SortedList<>(fileDetailList,
+                Comparator.comparing(FileCardDetails::lastModifiedTime, Comparator.reverseOrder()));
+        fileListView.setItems(sortedFileDetails);
+        fileListView.setCellFactory(listView -> new FileListPanel.FileListViewCell());
+
+        // Refresh all visible cells when any file changes
+        fileDetailList.addListener((ListChangeListener<FileCardDetails>) change -> {
+            fileListView.refresh();
+        });
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code File} using a {@code FileCard}.
+     */
+    class FileListViewCell extends ListCell<FileCardDetails> {
+        @Override
+        protected void updateItem(FileCardDetails details, boolean empty) {
+            super.updateItem(details, empty);
+
+            if (empty || details == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new FileCard(details).getRoot());
+            }
+        }
+    }
+
+    public FileCardDetails getFileCardDetails(File file) throws Exception {
+        int contactCount;
+        Path filePath = file.toPath();
+        try {
+            Optional<ReadOnlyAddressBook> addressBookOptional = new JsonAddressBookStorage(filePath).readAddressBook();
+            if (!addressBookOptional.isPresent()) {
+                throw new Exception("AddressBook file " + filePath + " not found");
+            }
+            contactCount = addressBookOptional.get().getContactList().size();
+        } catch (DataLoadingException e) {
+            throw e;
+        }
+
+        LocalDateTime lastModifiedTime = Instant.ofEpochMilli(file.lastModified()).atZone(ZONE_ID).toLocalDateTime();
+        return new FileCardDetails(file.getName(), lastModifiedTime, contactCount);
+    }
+
+    private void setUpWatcher(Path directory) throws IOException {
         directory.register(watchService,
                 StandardWatchEventKinds.ENTRY_CREATE,
                 StandardWatchEventKinds.ENTRY_MODIFY,
@@ -64,24 +125,28 @@ public class FileListPanel extends UiPart<Region> {
                         WatchEvent.Kind<?> kind = event.kind();
 
                         if (kind == StandardWatchEventKinds.OVERFLOW) {
+                            logger.warning("File listener received an overflow event.");
                             continue;
                         }
 
-                        Path changed = directory.resolve((Path) event.context());
-                        File changedFile = changed.toFile();
-
-                        Platform.runLater(() -> {
-                            if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
-                                fileList.add(changedFile);
-                            } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
-                                fileList.removeIf(f -> f.equals(changedFile));
-                            } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
-                                int index = fileList.indexOf(changedFile);
-                                if (index >= 0) {
-                                    fileList.set(index, changedFile);
+                        Path alteredPath = directory.resolve((Path) event.context());
+                        try {
+                            FileCardDetails change = getFileCardDetails(alteredPath.toFile());
+                            Platform.runLater(() -> {
+                                if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+                                    fileDetailList.add(change);
+                                } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
+                                    fileDetailList.removeIf(f -> f.equals(change));
+                                } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+                                    int index = fileDetailList.indexOf(change);
+                                    if (index >= 0) {
+                                        fileDetailList.set(index, change);
+                                    }
                                 }
-                            }
-                        });
+                            });
+                        } catch (Exception e) {
+                            logger.warning(e.getMessage());
+                        }
                     }
 
                     // directory no longer accessible
@@ -93,49 +158,5 @@ public class FileListPanel extends UiPart<Region> {
                 Thread.currentThread().interrupt();
             }
         });
-
-        FilteredList<File> filteredFiles = new FilteredList<>(fileList, this::isValidFile);
-        SortedList<File> sortedFiles = new SortedList<>(filteredFiles,
-                Comparator.comparing(File::lastModified, Comparator.reverseOrder()));
-        fileListView.setItems(sortedFiles);
-        fileListView.setCellFactory(listView -> new FileListPanel.FileListViewCell());
-
-        // Refresh all visible cells when any file changes
-        fileList.addListener((ListChangeListener<File>) change -> {
-            fileListView.refresh();
-        });
-    }
-
-    /**
-     * Custom {@code ListCell} that displays the graphics of a {@code File} using a {@code FileCard}.
-     */
-    class FileListViewCell extends ListCell<File> {
-        @Override
-        protected void updateItem(File file, boolean empty) {
-            super.updateItem(file, empty);
-
-            if (empty || file == null) {
-                setGraphic(null);
-                setText(null);
-            } else {
-                setGraphic(new FileCard(file).getRoot());
-            }
-        }
-    }
-
-    /**
-     * Checks if a given file is a valid B2B4U .json file.
-     */
-    public boolean isValidFile(File file) {
-        JsonAddressBookStorage addressBookStorage = new JsonAddressBookStorage(Path.of(file.getPath()));
-        try {
-            Optional<ReadOnlyAddressBook> addressBookOptional = addressBookStorage.readAddressBook();
-            if (!addressBookOptional.isPresent()) {
-                return false;
-            }
-            return true;
-        } catch (DataLoadingException e) {
-            return false;
-        }
     }
 }
